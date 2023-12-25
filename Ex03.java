8e14a99ebc324f06128f695be272363410ae9b65
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-26 00:12:04 +0800

    Update Ex03.java

--- Ex03.java
+++ Ex03.java
@@ -1,1 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Ex03 {
+    public static void main(String[] args) {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        try{
+            System.out.print("Input an integer n: ");
+            int n = Integer.parseInt(buf.readLine());
+            int sum = 0;
+
+            for (int i=1;i<=n;i++){
+                sum += i;
+            }
+            System.out.println("sum of 1 to " + n + " = " + sum);
+        } catch (IOException | NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+}
