0b1d5304ad04243c30f99ef8ab2763e5e31d2697
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-26 00:44:55 +0800

    Update Ex04.java

--- Ex04.java
+++ Ex04.java
@@ -1,1 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 
+public class Ex04 {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("donkey.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            int charCount = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                charCount += line.length(); 
+            }
+
+            bufferedReader.close();
+
+            System.out.println("Total characters read: " + charCount);      // (b)
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
