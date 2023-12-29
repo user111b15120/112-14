7c1bd8ec19c9b38a9c3fb8a15280bc7eea7df431
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-29 10:27:43 +0800

    Update Ex06.java

--- Ex06.java
+++ Ex06.java
@@ -1,1 +1,17 @@
+import java.io.IOException;
+import java.io.FileWriter;
 
+public class Ex06 {
+
+    public static void main(String[] args) {
+        char[] hi = {'h', 'e', 'l', 'l', 'o', ' ', 'J', 'a', 'v', 'a', '!', '\r', '\n'};
+        
+        try{
+            FileWriter fileWriter = new FileWriter("hello.txt");     
+            fileWriter.write(hi);
+            fileWriter.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
